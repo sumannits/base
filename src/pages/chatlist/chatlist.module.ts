b28d0e6c47d9cd6be306c94497c837a3ccb5b025
sumannits <suman@natitsolved.com>
2018-07-16 20:06:16 +0530

    chat count and list

--- src/pages/chatlist/chatlist.module.ts
+++ src/pages/chatlist/chatlist.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChatlistPage } from './chatlist';
+import { PipeModule } from "../../providers/pipe.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ChatlistPage } from './chatlist';
   ],
   imports: [
     IonicPageModule.forChild(ChatlistPage),
+    PipeModule
   ],
 })
 export class ChatlistPageModule {}
