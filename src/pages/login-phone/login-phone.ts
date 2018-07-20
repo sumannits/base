51a0d99c3f36814a194e5bc6bba6b0e23a48a0c9
suvayan00 <suvayan@natitsolved.com>
2018-07-20 20:05:04 +0530

    update

--- src/pages/login-phone/login-phone.ts
+++ src/pages/login-phone/login-phone.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, AlertController,ModalController,LoadingController,NavParams } from 'ionic-angular';
+import { Api } from '../../providers';
+import { FormControl, FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 
 /**
  * Generated class for the LoginPhonePage page.
@@ -14,12 +16,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login-phone.html',
 })
 export class LoginPhonePage {
+  private form: FormGroup;
+  public phone: AbstractControl;
+  public isd:AbstractControl;
+  public isFrmValid:boolean = true;
+  constructor(
+    public navCtrl: NavController,
+     public navParams: NavParams,
+     private fbuilder: FormBuilder,
+     public alertCtrl: AlertController,
+     public toastCtrl: ToastController,
+     public modalCtrl: ModalController,
+     public loadingCtrl:LoadingController,
+     public serviceApi: Api
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ) {
+    this.form = fbuilder.group({
+      'phone': ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{10}')])],
+      'isd' : ['', Validators.compose([Validators.required,Validators.minLength(3),Validators.maxLength(3),Validators.pattern('^[0-9+]*$'),Validators.required])]
+    });
+    this.phone = this.form.controls['phone'];
+    this.isd = this.form.controls['isd'];
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPhonePage');
   }
+
+ 
+
+  tost_message(msg){
+    let toast = this.toastCtrl.create({
+     message: msg,
+     duration: 3000
+   });
+   toast.present(); 
+    }
+
+
 
 }
